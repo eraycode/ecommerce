fc836e7870eb2abc45a9e20d493c682dbe8db063
eraycode <69942313+eraycode@users.noreply.github.com>
2021-12-25 18:02:16 +0100

    added shopping cart

--- main.js
+++ main.js
@@ -77,25 +77,23 @@ let products = [
 
 ];
 
-
-
 // grab all products
 for (let i=0; i < carts.length; i++) {
   carts[i].addEventListener('click', () => {
-    cartNumbers();
+    cartNumbers(products[i]);
+    totalCost(products[i])
   })
 }
 
 // function to load existing localStorage cart items
 function onLoadCartNumbers() {
   let productNumbers = localStorage.getItem('cartNumbers');
-
   if(productNumbers) {
     document.querySelector('.cartspan span').textContent = productNumbers;
   }
 }
 
-function cartNumbers() {
+function cartNumbers(product) {
   let productNumbers = localStorage.getItem('cartNumbers');
   productNumbers = parseInt(productNumbers);
 
@@ -107,6 +105,82 @@ function cartNumbers() {
     localStorage.setItem('cartNumbers', 1)
     document.querySelector('.cartspan span').textContent = 1;
   }
+  setItems(product)
 }
+
+function setItems(product) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  if(cartItems != null) {
+    if(cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product
+      }
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product
+    }
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(grabbedProduct) {
+    // console.log("The product costs", product.price);
+    let cartCost = localStorage.getItem("totalCost");
+    console.log("My cart cost is", cartCost);
+    console.log(typeof cartCost);
+
+    if (cartCost != null) {
+      cartCost = parseInt(cartCost);
+      localStorage.setItem("totalCost", cartCost + grabbedProduct.price);
+    } else {
+      localStorage.setItem("totalCost", grabbedProduct.price);
+    }
+}
+
+/* winnkelwagen js*/
+
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItem);
+  let productContainer = document.querySelector(".products-container");
+
+
+  if(cartItems && productContainer ) {
+    productContainer.innerHTML = "";
+    Object.values(cartItems).map(item => {
+      productContainer.innerHTML += `
+      <div class="product">
+        <ion-icon name="close-circle"></ion-icon>
+        <img src="./images/${item.tag}.jpg">
+        <span>${item.name}</span>
+        </div>
+      `
+
+    })
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 onLoadCartNumbers();
